6f5e32f8902d8ab47f83341a95c95dab30cb8cdb
philipdoucla <philipminhdo@ucla.edu>
2024-08-21 17:05:22 -0700

    Update resolutation to 720p

--- generate_rgbd.py
+++ generate_rgbd.py
@@ -7,8 +7,8 @@ pipeline = rs.pipeline()
 config = rs.config()
 
 # Enable the depth and color streams
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
 
 # Start streaming
 pipeline.start(config)
